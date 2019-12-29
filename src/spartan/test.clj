8a12406b14396be06aa32969fb46afafb38ffd06
Michiel Borkent <michielborkent@gmail.com>
2019-12-29 16:09:38 +0100

    debug

--- src/spartan/test.clj
+++ src/spartan/test.clj
@@ -30,6 +30,7 @@
     (doseq [v tests]
       (require (symbol (namespace v))))
     (let [{:keys [:error :fail]} (impl/run-tests tests)]
+      (prn "EXIT CODE" (+ error fail))
       (System/exit (+ error fail)))))
 
 ;;;; Scratch
