c066d00eca14e6e6f9fa40fcbcc558d93cb62045
Michiel Borkent <michielborkent@gmail.com>
2019-12-29 16:45:25 +0100

    sparten.test v0.0.1

--- project.clj
+++ project.clj
@@ -1,1 +1,13 @@
-(defproject borkdude/spartan.test "0.0.1-SNAPSHOT")
+(defproject borkdude/spartan.test "0.0.1"
+  :description "A spartan test framework compatible with babashka."
+  :url "https://github.com/borkdude/spartan.test"
+  :scm {:name "git"
+        :url "https://github.com/borkdude/babashka"}
+  :license {:name "Eclipse Public License 1.0"
+            :url "http://opensource.org/licenses/eclipse-1.0.php"}
+  :source-paths ["src"]
+  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
+                                    :username :env/clojars_user
+                                    :password :env/clojars_pass
+                                    :sign-releases false}]])
